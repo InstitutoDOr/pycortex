eb5d994fcc5989a97a959a70c51b0dec1d42bac4
NeuroDebian User <brain@neurodebian>
2017-06-12 01:21:21 -0400

    Alignment for SUBJ005 adjusted manually, with Heschl gyrus artificially highlighted in reference image

--- IDOR/make_static_visualization_single_subject.py
+++ IDOR/make_static_visualization_single_subject.py
@@ -1,8 +1,9 @@
-
+import sys
+sys.path.insert(0, '/home/brain/pycortex')
 import cortex
 import cortex.dataset as dts
 
-ss = [1]
+ss = [5]
 for s in ss:
 	subject = 'PRJ1210_SUBJ00' + str(s)
 	my_min = -0.1
@@ -11,12 +12,15 @@ for s in ss:
 
 	ds = dts.Dataset()
 
-	dv1 = cortex.Volume('/home/brain/pycortex/IDOR/images/RESULTS/S' + str(s) + 'W_mean_PC1best100voxels_Applied2All.nii', subject, 'fullhead', description='PC1', vmin=my_min, vmax=my_max, cmap='RdBu_r')
-	ds.append(PC1=dv1)
+	dv1 = cortex.Volume('/home/brain/pycortex/IDOR/images/RESULTS/S' + str(s) + 'W_mean_PC1best300voxels_Applied2All.nii.gz', subject, 'fullhead', description='PC1', vmin=my_min, vmax=my_max, cmap='RdBu_r')
+	fn = 'S' + str(s)	
+	kwargs = {fn: dv1}
+	ds.append(**kwargs)
+
 
 	import cortex.webgl.view_adapted
 
-	cortex.webgl.view_adapted.make_static('S' + str(s) + '_meanPC1_100vx_html' , ds)
+	cortex.webgl.view_adapted.make_static('S' + str(s) + '_meanPC1_300vx_html' , ds)
 
 
 
